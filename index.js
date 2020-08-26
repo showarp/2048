46b7a1c6bf3dcc5446569e36a6f68b5a8bfc7354
Yangsantai <3073995737@qq.com>
2020-08-26 14:43:18 +0800

    master

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
     gd = document.querySelectorAll('.gd');
-    const mapd = [
-        [0,1,2,3],
-        [4,5,6,7],
-        [8,9,10,11],
-        [12,13,14,15],
-    ];
-    obj = {
-    sj : function (){
-            return suiji = Math.floor(Math.random()*gd.length); //随机生成的位置
-        }};
-    function sc(mp){            //生成位置?以及数量
-        div = document.createElement('div');
-        div.className = 'sbox';
-        gd[mp].appendChild(div);
-    }
-    // for(var x = 0; x <mapd.length; x++){
-    //     for(var y = 0; y <mapd[x].length; y++){
-    //         console.log(mapd[x][y]);
-    //         sc(mapd[x][y]);
-    //     }
-    // }
+    mapd = new Array(4);
+    for(i = 0; i <=3; i++){
+        mapd[i] = new Array(4);
+    };
+    var a = -1;
+    for(let x = 0 ;x<mapd.length ;x++){
+        for(let y = 0;y<mapd[x].length ;y++){
+            a+=1; 
+            mapd[x][y] = {              //包含各种信息 横坐标 纵坐标 是否有方块 第几个父元素
+                key : gd[a],
+                left : x,
+                top : y,
+                value:false
+            };
+        };
+    };
+        function sc(mp){
+            div = document.currentElement('div');
+            div.className = 'sbox';
+            gd[mp].appendChild(div);
+        }
+        sc(1);
+    gd.forEach(function(nm,ix){
+        if(gd[ix].querySelectorAll('div').length>1){
+            console.log(gd[ix]);
+        }
+    })
